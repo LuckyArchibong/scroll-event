da4f042c92acc242c63efe6ec09353ba41daafa7
Decrypt01 <lalucky22@gmail.com>
2021-06-30 13:47:22 +0000

    init

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+//offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
 // select span
@@ -10,7 +14,6 @@ const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener("click", function () {
-  // linksContainer.classList.toggle("show-links");
 
   const linksHeight = links.getBoundingClientRect().height;
   const containerHeight = linksContainer.getBoundingClientRect().height;
@@ -19,7 +22,6 @@ navToggle.addEventListener("click", function () {
   } else {
     linksContainer.style.height = 0;
   }
-  // console.log(linksContainer.getBoundingClientRect());
 });
 
 // ********** fixed navbar ************
@@ -47,13 +49,14 @@ window.addEventListener("scroll", function () {
 });
 
 // ********** smooth scroll ************
+
 // select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
-    // navigate to specific spot
+    // navigate to a specific spot
     const id = e.currentTarget.getAttribute("href").slice(1);
     const element = document.getElementById(id);
 
@@ -73,8 +76,7 @@ scrollLinks.forEach((link) => {
       left: 0,
       top: position,
     });
-    // close
+    // close and calculate heights
     linksContainer.style.height = 0;
   });
 });
-// calculate heights
